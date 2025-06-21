11cbbb8416e0e6c2e04f6b127d5a0cce0b70a8e0
Suraj-git123 <ingolesuraj53@gmail.com>
2025-06-21 17:28:32 +0530

    Update MiniProjectUserMgmtAppApplication.java

--- src/main/java/com/app/MiniProjectUserMgmtAppApplication.java
+++ src/main/java/com/app/MiniProjectUserMgmtAppApplication.java
@@ -1,14 +1,20 @@
 package com.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+@SpringBootApplication
+public class MiniProjectUserMgmtAppApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MiniProjectUserMgmtAppApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MiniProjectUserMgmtAppApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectUserMgmtAppApplication.class, args);
+    }
 }
 
